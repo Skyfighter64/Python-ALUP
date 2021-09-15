169ead19e2cf657fbde9120ed455233c580316ce
Florian <florian.gaspar2000@gmail.com>
2021-09-15 23:41:05 +0200

    Remove local folder

--- local/sender.py
+++
@@ -1,8 +0,0 @@
-from src.UdpConnection import UdpConnection
-
-
-dev = UdpConnection("127.0.0.1", 1025)
-dev.server_port = 1027
-dev.Connect()
-while True:
-    dev.Send("Hello there".encode("ascii"))
--- local/test.py
+++
@@ -1,9 +0,0 @@
-from src.UdpConnection import UdpConnection
-
-
-dev = UdpConnection("127.0.0.1", 1026)
-dev.server_port = 1025
-dev.Connect()
-while True:
-    #dev.Send("Hello there".encode("ascii"))
-    print(str(dev.Read(4).decode("ascii")))

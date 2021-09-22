248759608b4a85bccb769145114ff4f1b6568b5a
Florian <florian.gaspar2000@gmail.com>
2021-09-22 18:58:05 +0200

    Add examples

---
+++ examples/serial_alup.py
@@ -0,0 +1,31 @@
+from src.Device import Device
+from src.Frame import Command
+import time
+
+# create a new device object
+dev = Device()
+#establish the serial connection at port "COM9" with a BAUD of 115200
+dev.SerialConnect("COM9", 115200)
+
+# Send a color frame changing the first two leds
+print("--- Color Test ---")
+dev.SetColors([0xffffff, 0xabff15])
+dev.Send()
+
+# Send a color frame only setting the 2nd LED to white
+print("--- Clear Color Test ---")
+dev.SetCommand(Command.CLEAR)
+dev.offset = 1
+dev.SetColors([0xffffff])
+dev.Send()
+
+# Blink the builtin LED
+# NOTE: this example needs Command ID 4 assigned to toggle the LED
+print("--- Builtin LED blink ---")
+dev.SetCommand(Command.LED_BUILTIN);
+dev.Send()
+time.sleep(1)
+dev.SetCommand(Command.LED_BUILTIN);
+dev.Send()
+
+dev.Disconnect()
---
+++ examples/udp_alup.py
@@ -0,0 +1,25 @@
+from src.Device import Device
+from src.Frame import Command
+import time
+
+# connect
+dev = Device()
+dev.UdpConnect('192.168.178.105', 5012)
+
+#send a frame with some colors
+dev.SetColors([0xffffff, 0x00ff00])
+dev.Send()
+# send a frame with colors and a command
+dev.SetCommand(Command.CLEAR)
+dev.SetColors([0xffffff,0x1252ff, 0x824347])
+dev.Send()
+
+# send a command
+dev.SetCommand(Command.LED_BUILTIN);
+dev.Send()
+
+time.sleep(1);
+dev.SetCommand(Command.LED_BUILTIN);
+dev.Send()
+# disconnect
+dev.Disconnect()
